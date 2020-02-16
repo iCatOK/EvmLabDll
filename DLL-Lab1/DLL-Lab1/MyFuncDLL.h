1641567ff9e94046f291b7b782ea77983071d324
Kamil <kamafotoclub@gmail.com>
2020-02-16 12:31:38 +0500

    3 - add specificatons of math functions

---
+++ DLL-Lab1/DLL-Lab1/MyFuncDLL.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifdef DLL_EXPORTS
+#define DLL_API extern "C" __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+DLL_API double Add(double a, double b);
+
+DLL_API double Substract(double a, double b);
+
+DLL_API double Multiply(double a, double b);
+
+DLL_API double Divide(double a, double b);
