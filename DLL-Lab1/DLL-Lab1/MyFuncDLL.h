75feacc576c00fa60c9fa789a61a5f4b3f2533fe
Kamil <kamafotoclub@gmail.com>
2020-02-16 12:54:01 +0500

    4 - add functions bodies

--- DLL-Lab1/DLL-Lab1/MyFuncDLL.h
+++ DLL-Lab1/DLL-Lab1/MyFuncDLL.h
@@ -2,7 +2,7 @@
 #ifdef DLL_EXPORTS
 #define DLL_API extern "C" __declspec(dllexport)
 #else
-#define DLL_API __declspec(dllimport)
+#define DLL_API  __declspec(dllimport)
 #endif
 
 DLL_API double Add(double a, double b);
@@ -12,3 +12,4 @@ DLL_API double Substract(double a, double b);
 DLL_API double Multiply(double a, double b);
 
 DLL_API double Divide(double a, double b);
+
