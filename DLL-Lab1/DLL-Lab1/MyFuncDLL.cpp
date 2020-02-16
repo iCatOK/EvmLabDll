5646a35926f0b15f3879ca26809c9bd0d94697d5
Kamil <kamafotoclub@gmail.com>
2020-02-16 16:58:45 +0500

    add realization of dll that built in QT

--- DLL-Lab1/DLL-Lab1/MyFuncDLL.cpp
+++ DLL-Lab1/DLL-Lab1/MyFuncDLL.cpp
@@ -1,18 +1,18 @@
 #include "pch.h"
 #include "MyFuncDLL.h"
 
-extern "C" __declspec(dllexport) double Add(double a, double b) {
+double Add(double a, double b) {
 	return a + b;
 }
 
-extern "C" __declspec(dllexport) double Substract(double a, double b) {
+double Substract(double a, double b) {
 	return a - b;
 }
 
-extern "C" __declspec(dllexport) double Multiply(double a, double b) {
+double Multiply(double a, double b) {
 	return a * b;
 }
 
-extern "C" __declspec(dllexport) double Divide(double a, double b) {
+double Divide(double a, double b) {
 	return a / b;
 }
---
+++ DLL-Lab1/DllRealization/DllRealization.cpp
@@ -0,0 +1,28 @@
+﻿// DllRealization.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <libloaderapi.h>
+#include <Windows.h>
+#include <string>
+
+typedef int (*pAdd) (int a, int b);
+
+using namespace std;
+
+int main()
+{
+	HINSTANCE hInstance = LoadLibrary(L"D:\\DEV\\C++\\Projects\\4 sem\\EVM-test\\Dll-exersises\\testDllWidget\\testLibrary\\testLibrary.dll");
+
+	if (!hInstance) {
+		cout << "Fuck yourself!";
+	}
+
+	pAdd add = (pAdd)GetProcAddress(hInstance, "_ZN11TestLibrary3AddEii");
+
+	if (add) {
+		cout << "God said : ' Yeas '.\n\n";
+	}
+
+	cout << add(5, 2);
+}
